b9fc32bbd5815b7aafec0f7862a10ab8162d4353
Alex Knopp <coda@knoppys.co.uk>
2017-02-02 13:25:02 +0000

    New media sizes for email marketing

--- custom/functions-wpmenu.php
+++ custom/functions-wpmenu.php
@@ -9,7 +9,7 @@ function remove_menus(){
   remove_menu_page( 'themes.php' );
   //remove_menu_page( 'plugins.php' );
   remove_menu_page( 'tools.php' );
-  remove_menu_page( 'options-general.php' ); 
+  //remove_menu_page( 'options-general.php' ); 
   remove_menu_page( 'edit.php?post_type=acf' );  
   
 }
