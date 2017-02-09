02762061e0359f7827179fbe8c1b0320bf89de4b
Alex Knopp <coda@knoppys.co.uk>
2017-02-09 13:32:20 +0000

    Bug fix to location field in booking edit screen

--- custom/admin-menu-corporatelistings.php
+++ custom/admin-menu-corporatelistings.php
@@ -98,7 +98,6 @@ function corporatelistings_callback() {
 							$apartmentobject = get_page_by_title( $bookingmeta['apartmentname'][0], OBJECT, 'apartments' );
 							echo get_post_meta($apartmentobject->ID,'apptlocation1',true);
 						}
-
 						?></td>
 						<td><?php echo $numberofnights; ?></td>
 						<td><?php echo $bookingmeta['apartmentname'][0] ?></td>
